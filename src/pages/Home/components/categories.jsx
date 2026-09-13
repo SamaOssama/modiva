0c250ca43f665da92b0a4e5c6585fe33d1eaff6b
Sama Osama <samaosma2005@gmail.com>
2026-09-14 02:45:25 +0300

    ksk

--- src/pages/Home/components/categories.jsx
+++ src/pages/Home/components/categories.jsx
@@ -1,13 +1,11 @@
 export default function Categories() {
-  const baseUrl = import.meta.env.BASE_URL;
-
   return (
     <section className="w-full px-12 py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-9">
         <div className="flex flex-col gap-9">
           <div className="relative h-93.5 overflow-hidden cursor-pointer">
             <img
-              src={`${baseUrl}assets/images/women.jpg`}
+              src="/assets/images/women.jpg"
               alt="Formal Woman"
               className="w-full h-full object-cover object-top"
             />
@@ -19,7 +17,7 @@ export default function Categories() {
 
           <div className="relative h-93.5 overflow-hidden cursor-pointer">
             <img
-              src={`${baseUrl}assets/images/men.jpg`}
+              src="/assets/images/men.jpg"
               alt="Formal Men"
               className="w-full h-full object-cover"
             />
@@ -32,7 +30,7 @@ export default function Categories() {
 
         <div className="relative h-195 overflow-hidden cursor-pointer">
           <img
-            src={`${baseUrl}assets/images/Rectangle 133.png`}
+            src="/assets/images/Rectangle 133.png"
             alt="Casual Style"
             className="w-full h-full object-cover"
           />
